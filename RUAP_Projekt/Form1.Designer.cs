e07ba107d2237593def4211dede94fab907891f0
fabijan50 <fabijanabjanovic@yahoo.com>
2023-06-30 15:04:31 +0200

    ruap projekt

---
+++ RUAP_Projekt/Form1.Designer.cs
@@ -0,0 +1,340 @@
+﻿
+namespace RUAP_Projekt
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBoxPh_Value = new System.Windows.Forms.TextBox();
+            this.textBoxHardness_Value = new System.Windows.Forms.TextBox();
+            this.textBoxSolids_Value = new System.Windows.Forms.TextBox();
+            this.textBoxChloramines_Value = new System.Windows.Forms.TextBox();
+            this.textBoxSulfate_Value = new System.Windows.Forms.TextBox();
+            this.textBoxConductivity_Value = new System.Windows.Forms.TextBox();
+            this.textBoxOrganicCarbon_Value = new System.Windows.Forms.TextBox();
+            this.textBoxTrihalomethanes_Value = new System.Windows.Forms.TextBox();
+            this.textBoxTurbidity_Value = new System.Windows.Forms.TextBox();
+            this.buttonRandom = new System.Windows.Forms.Button();
+            this.buttonGetResults = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.lblresult = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 57);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "Hardness";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 92);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.Text = "Solids";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(12, 127);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 3;
+            this.textBox4.Text = "Chloramines";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(12, 162);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 4;
+            this.textBox5.Text = "Sulfate";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(12, 197);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 5;
+            this.textBox6.Text = "Conductivity";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(12, 232);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.ReadOnly = true;
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 6;
+            this.textBox7.Text = "Organic carbon";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(12, 267);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.ReadOnly = true;
+            this.textBox8.Size = new System.Drawing.Size(100, 20);
+            this.textBox8.TabIndex = 7;
+            this.textBox8.Text = "Trihalomethanes";
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(12, 303);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.ReadOnly = true;
+            this.textBox9.Size = new System.Drawing.Size(100, 20);
+            this.textBox9.TabIndex = 8;
+            this.textBox9.Text = "Turbidity";
+            // 
+            // textBoxPh_Value
+            // 
+            this.textBoxPh_Value.Location = new System.Drawing.Point(118, 22);
+            this.textBoxPh_Value.Name = "textBoxPh_Value";
+            this.textBoxPh_Value.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPh_Value.TabIndex = 10;
+            this.textBoxPh_Value.Text = "1";
+            // 
+            // textBoxHardness_Value
+            // 
+            this.textBoxHardness_Value.Location = new System.Drawing.Point(118, 57);
+            this.textBoxHardness_Value.Name = "textBoxHardness_Value";
+            this.textBoxHardness_Value.Size = new System.Drawing.Size(100, 20);
+            this.textBoxHardness_Value.TabIndex = 11;
+            this.textBoxHardness_Value.Text = "1";
+            // 
+            // textBoxSolids_Value
+            // 
+            this.textBoxSolids_Value.Location = new System.Drawing.Point(118, 92);
+            this.textBoxSolids_Value.Name = "textBoxSolids_Value";
+            this.textBoxSolids_Value.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSolids_Value.TabIndex = 12;
+            this.textBoxSolids_Value.Text = "1";
+            // 
+            // textBoxChloramines_Value
+            // 
+            this.textBoxChloramines_Value.Location = new System.Drawing.Point(118, 127);
+            this.textBoxChloramines_Value.Name = "textBoxChloramines_Value";
+            this.textBoxChloramines_Value.Size = new System.Drawing.Size(100, 20);
+            this.textBoxChloramines_Value.TabIndex = 13;
+            this.textBoxChloramines_Value.Text = "1";
+            // 
+            // textBoxSulfate_Value
+            // 
+            this.textBoxSulfate_Value.Location = new System.Drawing.Point(118, 162);
+            this.textBoxSulfate_Value.Name = "textBoxSulfate_Value";
+            this.textBoxSulfate_Value.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSulfate_Value.TabIndex = 14;
+            this.textBoxSulfate_Value.Text = "1";
+            // 
+            // textBoxConductivity_Value
+            // 
+            this.textBoxConductivity_Value.Location = new System.Drawing.Point(118, 197);
+            this.textBoxConductivity_Value.Name = "textBoxConductivity_Value";
+            this.textBoxConductivity_Value.Size = new System.Drawing.Size(100, 20);
+            this.textBoxConductivity_Value.TabIndex = 15;
+            this.textBoxConductivity_Value.Text = "1";
+            // 
+            // textBoxOrganicCarbon_Value
+            // 
+            this.textBoxOrganicCarbon_Value.Location = new System.Drawing.Point(118, 232);
+            this.textBoxOrganicCarbon_Value.Name = "textBoxOrganicCarbon_Value";
+            this.textBoxOrganicCarbon_Value.Size = new System.Drawing.Size(100, 20);
+            this.textBoxOrganicCarbon_Value.TabIndex = 16;
+            this.textBoxOrganicCarbon_Value.Text = "1";
+            // 
+            // textBoxTrihalomethanes_Value
+            // 
+            this.textBoxTrihalomethanes_Value.Location = new System.Drawing.Point(118, 267);
+            this.textBoxTrihalomethanes_Value.Name = "textBoxTrihalomethanes_Value";
+            this.textBoxTrihalomethanes_Value.Size = new System.Drawing.Size(100, 20);
+            this.textBoxTrihalomethanes_Value.TabIndex = 17;
+            this.textBoxTrihalomethanes_Value.Text = "1";
+            // 
+            // textBoxTurbidity_Value
+            // 
+            this.textBoxTurbidity_Value.Location = new System.Drawing.Point(118, 303);
+            this.textBoxTurbidity_Value.Name = "textBoxTurbidity_Value";
+            this.textBoxTurbidity_Value.Size = new System.Drawing.Size(100, 20);
+            this.textBoxTurbidity_Value.TabIndex = 18;
+            this.textBoxTurbidity_Value.Text = "1";
+            // 
+            // buttonRandom
+            // 
+            this.buttonRandom.Location = new System.Drawing.Point(12, 334);
+            this.buttonRandom.Name = "buttonRandom";
+            this.buttonRandom.Size = new System.Drawing.Size(206, 92);
+            this.buttonRandom.TabIndex = 20;
+            this.buttonRandom.Text = "Random";
+            this.buttonRandom.UseVisualStyleBackColor = true;
+            this.buttonRandom.Click += new System.EventHandler(this.buttonRandom_Click);
+            // 
+            // buttonGetResults
+            // 
+            this.buttonGetResults.Location = new System.Drawing.Point(234, 303);
+            this.buttonGetResults.Name = "buttonGetResults";
+            this.buttonGetResults.Size = new System.Drawing.Size(310, 123);
+            this.buttonGetResults.TabIndex = 22;
+            this.buttonGetResults.Text = "Get Results";
+            this.buttonGetResults.UseVisualStyleBackColor = true;
+            this.buttonGetResults.Click += new System.EventHandler(this.buttonGetResults_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(234, 22);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(310, 186);
+            this.richTextBox1.TabIndex = 23;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "ph";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.PowderBlue;
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(550, 36);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(249, 372);
+            this.pictureBox1.TabIndex = 24;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.PowderBlue;
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.Location = new System.Drawing.Point(550, 36);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(249, 372);
+            this.pictureBox2.TabIndex = 25;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
+            // 
+            // lblresult
+            // 
+            this.lblresult.AutoSize = true;
+            this.lblresult.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblresult.Location = new System.Drawing.Point(242, 232);
+            this.lblresult.Name = "lblresult";
+            this.lblresult.Size = new System.Drawing.Size(0, 25);
+            this.lblresult.TabIndex = 26;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PowderBlue;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblresult);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.buttonGetResults);
+            this.Controls.Add(this.buttonRandom);
+            this.Controls.Add(this.textBoxTurbidity_Value);
+            this.Controls.Add(this.textBoxTrihalomethanes_Value);
+            this.Controls.Add(this.textBoxOrganicCarbon_Value);
+            this.Controls.Add(this.textBoxConductivity_Value);
+            this.Controls.Add(this.textBoxSulfate_Value);
+            this.Controls.Add(this.textBoxChloramines_Value);
+            this.Controls.Add(this.textBoxSolids_Value);
+            this.Controls.Add(this.textBoxHardness_Value);
+            this.Controls.Add(this.textBoxPh_Value);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.TextBox textBox6;
+		private System.Windows.Forms.TextBox textBox7;
+		private System.Windows.Forms.TextBox textBox8;
+		private System.Windows.Forms.TextBox textBox9;
+		private System.Windows.Forms.TextBox textBoxPh_Value;
+		private System.Windows.Forms.TextBox textBoxHardness_Value;
+		private System.Windows.Forms.TextBox textBoxSolids_Value;
+		private System.Windows.Forms.TextBox textBoxChloramines_Value;
+		private System.Windows.Forms.TextBox textBoxSulfate_Value;
+		private System.Windows.Forms.TextBox textBoxConductivity_Value;
+		private System.Windows.Forms.TextBox textBoxOrganicCarbon_Value;
+		private System.Windows.Forms.TextBox textBoxTrihalomethanes_Value;
+		private System.Windows.Forms.TextBox textBoxTurbidity_Value;
+		private System.Windows.Forms.Button buttonRandom;
+		private System.Windows.Forms.Button buttonGetResults;
+		private System.Windows.Forms.RichTextBox richTextBox1;
+		private System.Windows.Forms.TextBox textBox1;
+        public System.Windows.Forms.PictureBox pictureBox1;
+        public System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label lblresult;
+    }
+}
+
---
+++ RUAP_Projekt/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+
+namespace RUAP_Projekt
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+
+		public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonRandom_Click(object sender, EventArgs e)
+        {
+            textBoxPh_Value.Text = random.Next(0, 14).ToString();
+            textBoxHardness_Value.Text = random.Next(80, 300).ToString();
+            textBoxSolids_Value.Text = random.Next(8000, 50000).ToString();
+            textBoxChloramines_Value.Text = random.Next(3, 11).ToString();
+            textBoxSulfate_Value.Text = random.Next(250, 500).ToString();
+            textBoxConductivity_Value.Text = random.Next(200, 700).ToString();
+            textBoxOrganicCarbon_Value.Text = random.Next(5, 25).ToString();
+            textBoxTrihalomethanes_Value.Text = random.Next(5, 110).ToString();
+            textBoxTurbidity_Value.Text = random.Next(1, 10).ToString();
+        }
+
+        private async void buttonGetResults_Click(object sender, EventArgs e)
+        {
+            await InvokeRequestResponseService(textBoxPh_Value.Text, textBoxHardness_Value.Text, textBoxSolids_Value.Text, textBoxChloramines_Value.Text, 
+                textBoxSulfate_Value.Text,textBoxConductivity_Value.Text, textBoxOrganicCarbon_Value.Text, textBoxTrihalomethanes_Value.Text, textBoxTurbidity_Value.Text,
+                richTextBox1);
+        }
+
+        private async Task InvokeRequestResponseService(string ph, string hardness, string solids, string chloramines, string sulfate, string conductivity,
+            string organicCarbon, string trihalomethanes, string turbidity, RichTextBox richTextBox)
+        {
+            using (var client = new HttpClient())
+            {
+                var scoreRequest = new
+                {
+                    Inputs = new Dictionary<string, List<Dictionary<string, string>>>() {
+                        {
+                            "input1",
+                            new List<Dictionary<string, string>>(){new Dictionary<string, string>(){
+                                     {
+                                         "ph", ph
+                                     },
+                                     {
+                                         "Hardness", hardness
+                                     },
+                                     {
+                                         "Solids", solids
+                                     },
+                                     {
+                                         "Chloramines", chloramines
+                                     },
+                                     {
+                                         "Sulfate", sulfate
+                                     },
+                                     {
+                                         "Conductivity", conductivity
+                                     },
+                                     {
+                                         "Organic_carbon", organicCarbon
+                                     },
+                                     {
+                                         "Trihalomethanes", trihalomethanes
+                                     },
+                                     {
+                                         "Turbidity", turbidity
+                                     },
+                                     {
+                                         "Potability", "0"
+                                     },
+                                }
+                            }
+                        },
+                    },
+                    GlobalParameters = new Dictionary<string, string>()
+                    {
+                    }
+                };
+
+                const string apiKey = "qiEoLzmrkRO74eBx6oLxIrt9jAkJbEqm7chvhcddgQvJUKm2SH6/aLVHnH8CSG3K+J+r6gDGYd+G+AMCBzq3MA=="; // Replace this with the API key for the web service
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiKey);
+                client.BaseAddress = new Uri("https://ussouthcentral.services.azureml.net/workspaces/290841c73b2340499f63db5000f97aca/services/ad91691ec5894e99a68c8d7b2ac35369/execute?api-version=2.0&format=swagger");
+
+
+                HttpResponseMessage response = await client.PostAsJsonAsync("", scoreRequest);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    richTextBox.Clear();
+                    string result = await response.Content.ReadAsStringAsync();
+                    var formattedResult = JsonConvert.SerializeObject(JsonConvert.DeserializeObject(result), Formatting.Indented);
+                    Console.WriteLine("Result: {0}", formattedResult);
+
+                    richTextBox.AppendText("Result:" + Environment.NewLine);
+                    richTextBox.AppendText(formattedResult);
+
+                    if (formattedResult.Contains("\"Scored Labels\": \"1\""))
+					{
+                        Console.WriteLine("Water with entered properties is drinkable!");
+                        lblresult.Text = String.Format("Water with entered properties \n is drinkable!");
+                        lblresult.ForeColor = Color.Green;
+                        this.pictureBox1.Visible = true;
+                        this.pictureBox2.Visible = false;
+
+                    }
+					else
+					{
+                        Console.WriteLine("Water with entered properties is not drinkable!");
+                        lblresult.Text = String.Format("Water with entered properties \n is not drinkable!");
+                        lblresult.ForeColor = Color.Red;
+                        //richTextBox.AppendText("\n\n Water with entered properties is not drinkable!");
+                        this.pictureBox2.Visible = true;
+                        this.pictureBox1.Visible = false;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine(string.Format("The request failed with status code: {0}", response.StatusCode));
+                    Console.WriteLine(response.Headers.ToString());
+                    string responseContent = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(responseContent);
+                }
+            }
+        }
+    }
+}
